07312dafe481668993f9b532354a39521d0e060e
HarrySeop <parkpjs3083@naver.com>
2024-01-18 06:11:17 +0900

    💄Style: assets 폴더 추가
    파비콘, 로고, 아이콘 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+  document.getElementById('root'),
+);
+
+// 아래 코드는 리액트 18.2.0 버전에서 사용하는 함수라 주석처리함
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// );
