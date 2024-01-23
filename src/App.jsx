f7576d1f9f1849f9067d13192f51f369a4e3baac
HarrySeop <parkpjs3083@naver.com>
2024-01-24 06:40:23 +0900

    🎨 Modify : Container 추가, path 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import styled from 'styled-components';
-import { RouterProvider } from 'react-router-dom';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
+import NavBar from './components/navBar/NavBar';
 // import Category from '../pages/category/Category';
 // import Search from '../pages/search/Search';
 // import MyPage from '../pages/mypage/MyPage';
 // import NavBar from '../components/NavBar';
 
 const Wrapper = styled.div`
+  position: relative;
+  background-color: #ffffff;
+  height: 100vh; // 페이지 로드 전 배경 테스트용 높이 설정
+`;
+
+const Container = styled.div`
+  width: 325px;
   background-color: #ffffff;
   height: 100vh; // 페이지 로드 전 배경 테스트용 높이 설정
 `;
@@ -16,9 +23,12 @@ const Wrapper = styled.div`
 const App = () => {
   return (
     <Wrapper>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-      </Routes>
+      <NavBar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Container>
     </Wrapper>
   );
 };
