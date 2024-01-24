9fda41e14ecfdb185b34b590c3f55e0c9d5b9b49
김주하 <101048129+laketree2@users.noreply.github.com>
2024-01-24 20:10:14 +0900

    :lipstick: Fix : ui수정
    이미지 가운데정렬

--- src/pages/home/Home.Style.jsx
+++ src/pages/home/Home.Style.jsx
@@ -1,12 +1,11 @@
 import styled from 'styled-components';
-import GurdeningImg from '../../assets/images/logo-gurdening-subtitle.png';
+import GurdeningImg from '../../assets/images/BannerLogo.png';
 
 export const StyledImage = styled.img`
-  max-width: 100%; // 이미지의 최대 너비를 설정
-  height: 24vh; // 높이를 자동으로 조절하여 비율을 유지
-  display: flex;
-  justify-content: center; // Horizontally center children
-  align-items: center; // Vertically center children
+  max-width: 50%; // 이미지의 최대 너비를 설정
+  height: auto; // 높이를 자동으로 조절하여 비율을 유지
+  margin-top: 10px;
+  margin-left: 31%;
 `;
 
 export const LogoImg = () => <StyledImage src={GurdeningImg} alt="Gurdening" />;
