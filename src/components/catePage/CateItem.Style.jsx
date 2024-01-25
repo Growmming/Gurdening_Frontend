60f41301cc90576eab2d8171728d7d9d022e608f
김주하 <101048129+laketree2@users.noreply.github.com>
2024-01-26 04:29:56 +0900

    :lipstick: Fix : ui 수정
    배경색/글씨색

--- src/components/catePage/CateItem.Style.jsx
+++ src/components/catePage/CateItem.Style.jsx
@@ -7,22 +7,22 @@ export const CateItemWrapper = styled.div`
   align-items: center;
   padding: 10px;
   margin: 5px;
-  // background-color: #677d22;
-  background-color: #d2d999;
+  background-color: #677d22;
+  // background-color: #d2d999;
   border-radius: 10px;
   cursor: pointer;
   transition: background-color 0.3s;
 
   &:hover {
-    background-color: #fef5a6; // 호버 상태의 배경색 변경
+    background-color: #fef5a6;
   }
 `;
 
 export const CateIconWrapper = styled.div`
-  width: 80px; // 아이콘 배경의 너비
-  height: 60px; // 아이콘 배경의 높이
-  background-color: #d2d999; // 아이콘 배경의 색상
-  border-radius: 10px; // 아이콘 배경의 둥근 모서리
+  width: 80px;
+  height: 60px;
+  background-color: #d2d999;
+  border-radius: 10px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -31,18 +31,19 @@ export const CateIconWrapper = styled.div`
 `;
 
 export const CateIcon = styled.img`
-  width: 60px; // 크기는 예시입니다. 실제 디자인에 맞게 조정해야 합니다.
+  width: 60px;
   height: 60px;
   // margin-right: 10px;
 `;
 
 export const CateTextWrapper = styled.div`
-  width: 100%; // 텍스트 컨테이너의 너비를 아이템 너비와 동일하게 설정
+  width: 100%;
   display: flex;
-  justify-content: center; // 텍스트를 중앙에 위치
+  justify-content: center; // 텍스트 중앙정렬
 `;
 
 export const CateText = styled.span`
-  font-size: 16px; // 폰트 사이즈는 예시입니다. 실제 디자인에 맞게 조정해야 합니다.
+  font-size: 25px;
   color: #333; // 폰트 색상
+  // color: #666;
 `;
