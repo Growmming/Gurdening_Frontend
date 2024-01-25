9e61b2ba478c66c0f2b2623273c82ffa2745467b
김주하 <101048129+laketree2@users.noreply.github.com>
2024-01-26 03:16:46 +0900

    :truck: Fix : login 페이지 이동
    파일생성

--- src/components/homeCategory/CateItem.jsx
+++ src/components/homeCategory/CateItem.jsx
@@ -23,26 +23,3 @@ const CateItem = ({ name, icon, path }) => {
 };
 
 export default CateItem;
-
-// import React from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import * as S from './CateItem.Style';
-
-// const CateItem = ({ name, icon, path }) => {
-//   const navigate = useNavigate();
-
-//   const handleItemClick = () => {
-//     navigate(path);
-//   };
-
-//   return (
-//     <S.CateItemWrapper onClick={handleItemClick}>
-//       <S.CateIconWrapper>
-//         <S.CateIcon src={icon} alt={`${name} icon`} />
-//       </S.CateIconWrapper>
-//       <S.CateText>{name}</S.CateText>
-//     </S.CateItemWrapper>
-//   );
-// };
-
-// export default CateItem;
--- src/components/homeCategory/CategoryList.jsx
+++ src/components/homeCategory/CategoryList.jsx
@@ -31,33 +31,3 @@ const CategoryList = () => {
 };
 
 export default CategoryList;
-
-// import React from 'react';
-// import CateItem from './CateItem';
-
-// // 카테고리 데이터 리스트
-// const categoryData = [
-//   { name: 'Cooking', icon: 'cook-icon.png', path: '/menual1' },
-//   { name: 'Daily_Life', icon: 'cook-icon.png', path: '/menual2' },
-//   { name: 'Cleanliness', icon: 'cook-icon.png', path: '/menual3' },
-//   { name: 'Treatment', icon: 'cook-icon.png', path: '/menual4' },
-//   { name: 'Health', icon: 'cook-icon.png', path: '/menual5' },
-//   { name: 'HomeEco', icon: 'cook-icon.png', path: '/menual6' },
-// ];
-
-// const CategoryList = () => {
-//   return (
-//     <div>
-//       {categoryData.map((category) => (
-//         <CateItem
-//           key={category.name}
-//           name={category.name}
-//           icon={category.icon}
-//           path={category.path}
-//         />
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default CategoryList;
